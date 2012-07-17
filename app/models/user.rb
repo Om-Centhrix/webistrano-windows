1c7f66c1214c3ac779e6a02e5af4802b2a522ee2
Kevin Morris <kmorris511@yahoo.com>
2012-07-16 20:14:33 -0600

    Reverting user.  Taking a different approach

--- app/models/user.rb
+++ app/models/user.rb
@@ -27,8 +27,9 @@ class User < ActiveRecord::Base
   end
   
   # Authenticates a user by their login name and unencrypted password.  Returns the user or nil.
-  def self.authenticate(login, password, strategy = AuthenticationStrategies::WebistranoStrategy)
-    strategy.authenticate(login, password)
+  def self.authenticate(login, password)
+    u = find_by_login_and_disabled(login, nil) # need to get the salt
+    u && u.authenticated?(password) ? u : nil
   end
 
   # Encrypts some data with the salt.
