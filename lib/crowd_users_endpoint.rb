d40bd6a8630ca04fc76b6d1e94b4837299bd2dc5
Kevin Morris <kmorris511@yahoo.com>
2012-12-19 17:55:08 -0700

    PTSYS-81: Making Crowd endpoint configurable via yml file

--- lib/crowd_users_endpoint.rb
+++ lib/crowd_users_endpoint.rb
@@ -1,18 +1,15 @@
 class CrowdUsersEndpoint
 
-  CROWD_APPLICATION_USERNAME = "webistrano"
-  CROWD_APPLICATION_PASSWORD = "foobar"
-  CROWD_REST_HOST = "localhost"
-
-  USER_INDEX_URL = "http://#{CrowdUsersEndpoint::CROWD_APPLICATION_USERNAME}:#{CrowdUsersEndpoint::CROWD_APPLICATION_PASSWORD}@#{CrowdUsersEndpoint::CROWD_REST_HOST}:8095/crowd/rest/usermanagement/1/search?entity-type=user"
-  USER_GET_URL = "http://#{CrowdUsersEndpoint::CROWD_APPLICATION_USERNAME}:#{CrowdUsersEndpoint::CROWD_APPLICATION_PASSWORD}@#{CrowdUsersEndpoint::CROWD_REST_HOST}:8095/crowd/rest/usermanagement/1/user?username=__login__&expand=attributes"
+  cattr_accessor :config
 
   def self.index
-    JSON.parse(RestClient.get(USER_INDEX_URL, {:accept => "application/json"}))
+    url = "http://#{config['application']}:#{config['password']}@#{config['host']}:#{config['port']}/crowd/rest/usermanagement/1/search?entity-type=user"
+    JSON.parse(RestClient.get(url, {:accept => "application/json"}))
   end
 
   def self.get(login)
-    url = USER_GET_URL.gsub("__login__", login)
+    url = "http://#{config['application']}:#{config['password']}@#{config['host']}:#{config['port']}/crowd/rest/usermanagement/1/user?username=__login__&expand=attributes"
+    url = url.gsub("__login__", login)
 
     begin
       JSON.parse(RestClient.get(url, {:accept => "application/json"}))
