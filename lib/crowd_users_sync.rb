60c5242b27efb52801480cf0eec50a2dec217e95
Kevin Morris <kmorris511@yahoo.com>
2013-01-02 11:01:26 -0700

    PTSYS-81: Checking for the existence of the users table before syncing

--- lib/crowd_users_sync.rb
+++ lib/crowd_users_sync.rb
@@ -5,7 +5,7 @@ module CrowdUsersSync
   CROWD_USER_SYNC_INTERVAL = '1h' #Every hour
 
   def sync
-    User.create_or_update_from_crowd_users(CrowdUsersEndpoint.index)
+    User.create_or_update_from_crowd_users(CrowdUsersEndpoint.index) if User.table_exists?
   end
   module_function(:sync)
 
