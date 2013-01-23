9931c93c9d035d6bc1164d590e9e9182eead5055
Kevin Morris <kmorris511@yahoo.com>
2013-01-23 08:52:44 -0700

    Adding more unit tests

--- test/unit/crowd_users_endpoint_test.rb
+++ test/unit/crowd_users_endpoint_test.rb
@@ -50,5 +50,78 @@ class CrowdUsersEndpointTest < ActiveSupport::TestCase
     actual = CrowdUsersEndpoint.get("foo")
     assert_equal(expected, actual)
   end
+ 
+  def test_authenticate_success
 
+    login = "foo"
+    password = "correct_password"
+
+    result = %({"expand":"attributes",
+                "link":{"href":"https://localhost:8443/crowd/rest/usermanagement/1/user?username=foo","rel":"self"},
+                "name":"foo","first-name":"Foo","last-name":"Bar","display-name":"Foo Bar","email":"foo@bar.com",
+                "password":{"link":{"href":"https://localhost:8443/crowd/rest/usermanagement/1/user/password?username=foo","rel":"edit"}},"active":true,
+                "attributes":{"attributes":[],"link":{"href":"https://localhost:8443/crowd/rest/usermanagement/1/user/attribute?username=foo","rel":"self"}}})
+
+    expected = {"expand" => "attributes",
+                "link" => {"href" => "https://localhost:8443/crowd/rest/usermanagement/1/user?username=foo","rel" => "self"},
+                "name" => "foo","first-name" => "Foo","last-name" => "Bar","display-name" => "Foo Bar","email" => "foo@bar.com",
+                "password" => {"link" => {"href" => "https://localhost:8443/crowd/rest/usermanagement/1/user/password?username=foo","rel" => "edit"}},"active" => true,
+                "attributes" => {"attributes" => [],"link" => {"href" => "https://localhost:8443/crowd/rest/usermanagement/1/user/attribute?username=foo","rel" => "self"}}}
+
+    mock = flexmock(RestClient)
+    mock.should_receive(:post).times(1).and_return(result)
+
+    actual = CrowdUsersEndpoint.authenticate(login, password)
+    assert_equal(expected, actual)                  
+  end
+
+
+  def test_authenticate_failure
+
+    login = "foo"
+    password = "incorrect_password"
+
+    expected = nil
+
+    mock = flexmock(RestClient)
+    mock.should_receive(:post).times(1).and_raise(RestClient::BadRequest)
+
+    actual = CrowdUsersEndpoint.authenticate(login, password)
+    assert_equal(expected, actual)    
+  end
+
+
+  def test_update_password
+
+    login = "foo"
+    password = "correct_password"
+
+    result = Hash.new
+    expected = Hash.new
+
+    mock = flexmock(RestClient)
+    mock.should_receive(:put).times(1).and_return(result)
+
+    actual = CrowdUsersEndpoint.update_password(login, password)
+    assert_equal(expected, actual)
+    
+  end
+
+  def test_groups
+
+    login = "foo"
+  
+    result = %({"expand":"group",
+                "groups":[{"link":{"href":"https://localhost:8443/crowd/rest/usermanagement/1/group?groupname=crowd-administrators","rel":"self"},"name":"crowd-administrators"}]})
+  
+    expected = {"expand" => "group",
+                "groups" => [{"link" => {"href" => "https://localhost:8443/crowd/rest/usermanagement/1/group?groupname=crowd-administrators","rel" => "self"},"name" => "crowd-administrators"}]}
+    
+    mock = flexmock(RestClient)
+    mock.should_receive(:get).times(1).and_return(result)
+
+    actual = CrowdUsersEndpoint.groups(login)
+    assert_equal(expected, actual)
+
+  end
 end
